7d93f19c48032a8e68c2608964f6a9e0faa8aaa2
pOsdas <ratmir1099@mail.ru>
2024-08-21 22:49:35 +0300

    minor update

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # main_second.py
+# import os
+# os.environ['KIVY_ANGLE_DIR'] = r'C:\Users\Maloy\PycharmProjects\Download (GitHub)\.venv\share\angle\bin'
+import time
+
 from kivy.core.window import Window
 import ctypes
 from ctypes import wintypes
 
 from classes import YoutubeDownloaderApp
+
+import os
+import certifi
+os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+
 
 # Window.size = (465, 1000)
 # Window.top = 30
@@ -14,17 +23,17 @@ from classes import YoutubeDownloaderApp
 # Window.maximum_width = 465
 # Window.maximum_height = 1010
 
-SPI_GETWORKAREA = 48
+SPI_GETWORKAREA: int = 48
 
 rect = wintypes.RECT()
 
 ctypes.windll.user32.SystemParametersInfoW(SPI_GETWORKAREA, 0, ctypes.byref(rect), 0)
-screen_height = ctypes.windll.user32.GetSystemMetrics(1)
-taskbar_height = screen_height - (rect.bottom - rect.top)
+screen_height: int = ctypes.windll.user32.GetSystemMetrics(1)
+taskbar_height: int = screen_height - (rect.bottom - rect.top)
 user32 = ctypes.windll.user32
-screen_width = user32.GetSystemMetrics(0)
+screen_width: int = user32.GetSystemMetrics(0)
 
-window_height = screen_height - taskbar_height - 30
+window_height: int = screen_height - taskbar_height - 30
 Window.size = (465, window_height)
 Window.left = screen_width - 465
 Window.top = 30
@@ -42,6 +51,12 @@ Window.maximum_width = 465
 Window.minimum_height = window_height
 Window.maximum_height = window_height
 
+
 if __name__ == "__main__":
     YoutubeDownloaderApp().run()
+
+
+
+
+
 
